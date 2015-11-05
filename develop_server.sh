765e45436ab2e75e7a3caaa07506b1f54afa8b1c
Marvin Teichmann <marvin.teichmann@googlemail.com>
2015-11-06 00:37:15 +0100

    update develop_server to use local conf

--- develop_server.sh
+++ develop_server.sh
@@ -9,7 +9,7 @@ PELICANOPTS=
 BASEDIR=$(pwd)
 INPUTDIR=$BASEDIR/content
 OUTPUTDIR=$BASEDIR/output
-CONFFILE=$BASEDIR/pelicanconf.py
+CONFFILE=$BASEDIR/pelicanconf-local.py
 
 ###
 # Don't change stuff below here unless you are sure
